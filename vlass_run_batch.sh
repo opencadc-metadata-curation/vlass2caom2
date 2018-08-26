eae24eceda17e8be5c54775ca3095fdeb2c7eca0
Dustin Jenkins <Dustin.Jenkins@nrc-cnrc.gc.ca>
2018-08-25 19:30:32 -0700

    Add batch runner for batch inputs.

--- vlass_run_batch.sh
+++ vlass_run_batch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ARG_LEN=$(($#-1))
-
 # Hack to get last argument.
 for CERTIFICATE_STRING; do true; done
 
 # Everything before certificate is an input.
-INPUTS=${@:1:$ARG_LEN}
+ARG_LEN=$(($#-1))
+INPUTS=(${@:1:$ARG_LEN})
 
-for INPUT_FILE in "${INPUTS[@]}"; do
+for INPUT_FILE in "${INPUTS[@]}";
+do
   if [[ ! -z "${INPUT_FILE}" ]];
   then
     echo "Processing ${INPUT_FILE}."
