896d42ff1db2ccec8a5eb2789c773cd7da0d02a8
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2019-05-09 13:15:05 -0700

    Update vlass_run_visitor.sh
    Use bucket.canfar.net, and expressly pull the image.

--- scripts/vlass_run_visitor.sh
+++ scripts/vlass_run_visitor.sh
@@ -6,12 +6,14 @@ then
     date
     exit -1
 fi
+echo "Get the container"
+docker pull bucket.canfar.net/vlass2caom2 || exit $?
 
 echo "Generate todo list"
 cadc-tap query --cert $HOME/.ssl/cadcproxy.pem -s ivo://cadc.nrc.ca/ams/cirada "SELECT A.uri FROM caom2.Observation AS O JOIN caom2.Plane AS P ON O.obsID = P.obsID JOIN caom2.Artifact AS A ON P.planeID = A.planeID WHERE O.collection='VLASS' AND P.provenance_reference is NULL" | awk -F\/ '{print $2}' >& todo.txt || exit $?
 
 echo "Run vlass_run container"
-docker run --rm --name vlass_run -v ${PWD}:/usr/src/app/ vlass_run vlass_run || exit $?
+docker run --rm --name vlass_run -v ${PWD}:/usr/src/app/ vlass2caom2 vlass_run || exit $?
 
 date
 exit 0
