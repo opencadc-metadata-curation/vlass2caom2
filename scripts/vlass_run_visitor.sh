a454be242a8ebe66f8d855902c8281d051029df7
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2019-05-13 08:24:44 -0700

    Update vlass_run_visitor.sh

--- scripts/vlass_run_visitor.sh
+++ scripts/vlass_run_visitor.sh
@@ -10,7 +10,7 @@ echo "Get the container"
 docker pull bucket.canfar.net/vlass2caom2 || exit $?
 
 echo "Generate todo list"
-cadc-tap query --cert $HOME/.ssl/cadcproxy.pem -s ivo://cadc.nrc.ca/ams/cirada "SELECT A.uri FROM caom2.Observation AS O JOIN caom2.Plane AS P ON O.obsID = P.obsID JOIN caom2.Artifact AS A ON P.planeID = A.planeID WHERE O.collection='VLASS' AND P.provenance_reference is NULL" | awk -F\/ '{print $2}' >& todo.txt || exit $?
+cadc-tap query --cert $HOME/.ssl/cadcproxy.pem -s ivo://cadc.nrc.ca/ams/cirada "SELECT A.uri FROM caom2.Observation AS O JOIN caom2.Plane AS P ON O.obsID = P.obsID JOIN caom2.Artifact AS A ON P.planeID = A.planeID WHERE O.collection='VLASS' AND P.provenance_reference is NULL" | awk -F\/ '{print $2}' | grep -v "^$" >& todo.txt || exit $?
 
 echo "Run vlass_run container"
 docker run --rm --name vlass_run -v ${PWD}:/usr/src/app/ bucket.canfar.net/vlass2caom2 vlass_run || exit $?
