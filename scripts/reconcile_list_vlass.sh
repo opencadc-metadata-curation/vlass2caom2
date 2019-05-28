a8ba3d3a00882ce3d3ebe372693f0c1b0cfa1694
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2019-05-27 17:53:22 -0700

    Update reconcile_list_vlass.sh

--- scripts/reconcile_list_vlass.sh
+++ scripts/reconcile_list_vlass.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-echo "Get a certificate"
+echo "Get a proxy certificate"
 cp $HOME/.ssl/cadcproxy.pem ./ || exit $?
 
 echo "Get the container"
-# docker pull bucket.canfar.net/vlass2caom2 || exit $?
+docker pull bucket.canfar.net/vlass2caom2 || exit $?
 
 echo "Run vlass_run container"
-# docker run --rm --name vlass_run -v ${PWD}:/usr/src/app/ bucket.canfar.net/vlass2caom2 python vlass_validator.py || exit $?
-docker run --rm --name vlass_run -v ${PWD}:/usr/src/app/ vlass_val_test python /usr/local/lib/python3.6/site-packages/vlass2caom2/vlass_validator.py || exit $?
+docker run --rm --name vlass_run -v ${PWD}:/usr/src/app/ bucket.canfar.net/vlass2caom2 python /usr/local/lib/python3.6/site-packages/vlass2caom2/vlass_validator.py || exit $?
 
 date
 exit 0
