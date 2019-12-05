756b0fff5a4ef3002fcc9b1ddb0afc64e3749494
Sharon Goliath <goliaths.cadc@gmail.com>
2019-12-04 17:21:16 -0800

    s2726 - upgrade to python 3.7.

--- scripts/reconcile_list_vlass.sh
+++ scripts/reconcile_list_vlass.sh
@@ -9,7 +9,7 @@ echo "Get the container"
 docker pull ${CONTAINER} || exit $?
 
 echo "Run vlass_run container"
-docker run --rm --name vlass_run -v ${PWD}:/usr/src/app/ ${CONTAINER} python /usr/local/lib/python3.6/site-packages/vlass2caom2/validator.py || exit $?
+docker run --rm --name vlass_run -v ${PWD}:/usr/src/app/ ${CONTAINER} python /usr/local/lib/python3.7/site-packages/vlass2caom2/validator.py || exit $?
 
 date
 exit 0
