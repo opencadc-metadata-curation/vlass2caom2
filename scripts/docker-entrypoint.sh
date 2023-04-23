d0ca9b955073f2d96a20b496f0c27a71270baba9
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2023-04-23 11:16:44 -0700

    CADC-12376 - use the URLs for the state.yml file content.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -9,7 +9,9 @@ if [[ ! -e ${PWD}/state.yml ]]
 then
   yesterday=$(date -d yesterday "+%d-%b-%Y %H:%M")
   echo "bookmarks:
-  vlass_timestamp:
+  https://archive-new.nrao.edu/vlass/quicklook/:
+    last_record: $yesterday
+  https://archive-new.nrao.edu/vlass/se_continuum_imaging/:
     last_record: $yesterday
 context:
   vlass_context:
@@ -17,6 +19,7 @@ context:
     VLASS1.2v2: 01-Nov-2018 00:00
     VLASS2.1: 01-Jul-2020 00:00
     VLASS2.2: 01-Sep-2021 00:00
+    VLASS3.1: 01-Jun-2022 00:00
 " > ${PWD}/state.yml
 fi
 
